5286b7c97aaadfc100d99e83b0f79657945a7fc9
unknown <gjoseesteban44@gmail.com>
2024-06-14 09:24:47 -0600

    Practicas

---
+++ CadenaTexto/CadenaTexto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadenaTexto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] arreglo = { "Alan", "Jona", "Carlos", "Emiliano", "Lucas" };
+            
+
+            Console.WriteLine("La primera casilla del arreglo es: " + arreglo[0]);
+
+            Console.ReadKey();  
+        }
+    }
+}
